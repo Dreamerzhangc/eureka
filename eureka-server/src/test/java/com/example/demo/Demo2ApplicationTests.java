da910c24aea315639087f1d765930e33cb277cca
issuser <issuser@ISS>
2019-12-13 17:44:49 +0800

    eureka注册中心项目

---
+++ eureka-server/src/test/java/com/example/demo/Demo2ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
